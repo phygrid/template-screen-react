415dc6a478c4103decf204afa18f2b43a6df11f0
Ahmed Abdelfatah <ahmed.m.web.dev@gmail.com>
2025-06-25 15:15:20 +0400

    refactor: dev mode

--- src/utils/dev-mode.ts
+++ src/utils/dev-mode.ts
@@ -1,11 +1,16 @@
 /**
  * This file is used to get the dev settings from src/settings/index.json file.
  */
-import Settings from "../schema";
+import Settings from '../schema';
 
 export const getDevSettings = (): Settings => {
-  const devSettings = require("../settings/index.json");
-  return devSettings.app.gridApp.settings as Settings;
+  try {
+    const devSettings = require('../settings/index.json');
+    return devSettings?.app?.gridApp?.settings;
+  } catch (err) {
+    console.warn('Failed to load dev settings:', err);
+  }
+  return {} as Settings;
 };
 
 export const isDevMode = (): boolean => {
