fc0bc7fb933ff559c94563c25ebfe83ce42aee78
Ahmed Abdelfatah <ahmed.m.web.dev@gmail.com>
2025-06-25 15:03:23 +0400

    refactor: removing the fallback settings

--- src/utils/dev-mode.ts
+++ src/utils/dev-mode.ts
@@ -1,23 +1,13 @@
 /**
  * This file is used to get the dev settings from src/settings/index.json file.
- * This can be used by running the app using this command: yarn run start:dev
  */
-import Settings from '../schema';
-
-const fallbackSettings: Settings = {
-  productName: "Default Product",
-  productPrice: "99 USD"
-};
+import Settings from "../schema";
 
 export const getDevSettings = (): Settings => {
-  try {
-    const devSettings = require('../settings/index.json');
-    return devSettings.app.gridApp.settings as Settings;
-  } catch {
-    return fallbackSettings;
-  }
+  const devSettings = require("../settings/index.json");
+  return devSettings.app.gridApp.settings as Settings;
 };
 
 export const isDevMode = (): boolean => {
-  return process.env.NODE_ENV === 'development';
-}; 
+  return process.env.NODE_ENV === "development";
+};
